07ad6128bf2d58f776dd7e9892496a6ecf7e7b74
Shawna <skjacques@gmail.com>
2022-07-24 14:01:29 -0700

    Fixed all failing tests.

--- mit.js
+++ mit.js
@@ -1,7 +1,7 @@
 var mit = {
     city: 'Cambridge',
-    colors: ['pink', 'red'],
-    mascot: "robot",
-    founded: 1900,
-    motto: 'Carpe diem',
+    colors: 'Silver Gray',
+    mascot: 'robot',
+    founded: 1875,
+    motto: 'Mens',
 };
